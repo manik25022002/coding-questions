25f1375ec01a680435e1975bd5f20099b54651c8
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-05-07 20:01:30 +0530

    Time: 11 ms (47.31%) | Memory: 20.6 MB (28.07%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+    
+        stack<int> st;
+        for (int i = 0; i < asteroids.size(); i++) {
+            int val = asteroids[i];
+            if (!st.empty() && st.top() > 0 && val < 0) {
+                if (abs(val) == abs(st.top())) {
+                    st.pop();
+                } else if (abs(val) > abs(st.top())) {
+                    st.pop();
+                    i--;
+                }
+            } else {
+                st.push(val);
+            }
+        }
+            vector<int> ans(st.size());
+        for(int i=st.size()-1;i>=0;i--){
+            ans[i]=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
