f8c9fb08a67e6b70b71b597965d201bd5fe84014
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-07-03 22:45:10 +0530

    Time: 14 ms (38.19%) | Memory: 20.4 MB (74.11%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> merge( vector<vector<int>>& inter) {
-        vector<vector<int>>final;
-        if (inter.size() == 0)
-            return final;
-        sort(inter.begin(), inter.end());
-        vector<int> temp = inter[0];
-        for (auto it : inter) {
-            if (it[0] <= temp[1]) {
-                temp[1] = max(it[1], temp[1]);
-            } else {
-                final.push_back(temp);
-                temp = it;
-            }
-        }
-        final.push_back(temp);
-        return final;
-    }
-
     vector<vector<int>> insert(vector<vector<int>>& intervals,
                                vector<int>& newInterval) {
-        vector<vector<int>> result;
-        int n = intervals.size();
+        vector<vector<int>> result1;
         int i = 0;
+        int n = intervals.size();
         while (i < n && intervals[i][1] < newInterval[0]) {
-            result.push_back(intervals[i]);
+            result1.push_back(intervals[i]);
             i++;
         }
-        while (i < n && intervals[i][0] < newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
+                 i++;
         }
-        result.push_back(newInterval);
-        while (i < n) {
-            result.push_back(intervals[i]);
-            i++;
+        result1.push_back(newInterval);
+        while(i<n){
+              result1.push_back(intervals[i]);
+              i++;
         }
-       return merge(result);
-      
+        return result1;
     }
 };
