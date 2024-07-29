690e8dc597a8053fbe495dca5aa8f90a0f4893ed
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-07-29 21:56:32 +0530

    Time: 5 ms (22.07%) | Memory: 7.5 MB (21.63%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+bool solve(int x){
+    if(x==1) return true;
+    if(x%4!=0)return false;
+    else{
+        x=x/4;
+        return solve(x);
+    }
+}
+    bool isPowerOfFour(int n) {
+        if(n<1) return false;
+        int ans= solve(n);
+        return ans;
+    }
+};
