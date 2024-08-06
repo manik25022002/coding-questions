3ea5ff03f0c193a27c13680b8a5ed64f6c659470
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-08-06 19:46:58 +0530

    Time: 2 ms (42.33%) | Memory: 10 MB (34.21%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -6,21 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-void solve(TreeNode* root, vector<int>&ans){
-    if(root == nullptr)return;
-    ans.push_back(root->val);
-    solve(root->left,ans);
-    solve(root->right,ans);
-}
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        solve(root,ans);
+        vector<int> ans;
+        if (root == nullptr)
+            return ans;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty()) {
+            root = s.top();
+            s.pop();
+            ans.push_back(root->val);
+             if (root->right != nullptr)
+                s.push(root->right);
+            if (root->left != nullptr)
+                s.push(root->left);
+        }
         return ans;
-
     }
 };
+
+// simple method using recursion only very easy
+// make function then call
+// root left right then
