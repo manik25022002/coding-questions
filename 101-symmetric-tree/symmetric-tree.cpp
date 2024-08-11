6f4eaba760074161a1f837a806eb4e6a6a9f0fd2
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-08-11 18:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (15.98%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -6,19 +6,20 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-bool ischecktree(TreeNode* left,TreeNode* right){
-if(left==nullptr && right==nullptr)return true;
-if(left==nullptr || right == nullptr)return false;
-if(left->val != right->val)return false;
-return  (ischecktree( left->left,right->right) && ischecktree(left->right,right->left));
-}
+    bool ischecktree(TreeNode* left, TreeNode* right) {
+        if (left == nullptr && right == nullptr)return true;
+        if (left == nullptr || right == nullptr)return false;
+        if (left->val != right->val)return false;
+        return (ischecktree(left->left, right->right)&&ischecktree(left->right, right->left));
+    }
     bool isSymmetric(TreeNode* root) {
-       if(root==nullptr)return true;
-       return (ischecktree(root->left,root->right));
+        if (root == nullptr)return true;
+        return (ischecktree(root->left, root->right));
     }
 };
