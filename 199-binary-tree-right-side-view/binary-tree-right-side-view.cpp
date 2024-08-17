16a8d6e8d4af235d7f443a68a5dcd7648a431160
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-08-17 17:02:20 +0530

    Time: 9 ms (5.97%) | Memory: 14.9 MB (9.12%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,8 +14,7 @@ class Solution {
 public:
     vector<vector<int>> level(TreeNode* root) {
         vector<vector<int>> ans;
-        if (root == nullptr)
-            return ans;
+        if (root == nullptr)return ans;
         queue<TreeNode*> q;
         q.push(root);
         while (!q.empty()) {
@@ -25,10 +24,8 @@ public:
                 TreeNode* node = q.front();
                 level.push_back(node->val);
                 q.pop();
-                if (node->left)
-                    q.push(node->left);
-                if (node->right)
-                    q.push(node->right);
+                if (node->left)q.push(node->left);
+                if (node->right)q.push(node->right);
             }
             ans.push_back(level);
         }
