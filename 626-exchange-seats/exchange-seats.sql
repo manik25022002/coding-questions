8f86e22db207e0a2fac9501afb211394eea9aaf3
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-08-27 23:01:44 +0530

    Time: 349 ms (39.25%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -4,6 +4,5 @@ case
 when id%2=0 then id-1
 when (id%2!=0 and id=(select max(Id) from seat))then id
 when id%2!=0 then id+1
-end 
-as id ,student from seat
+end as id ,student from seat
 order by id;
