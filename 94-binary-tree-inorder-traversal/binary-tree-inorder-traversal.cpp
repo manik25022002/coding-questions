3cd161a34157ec885ec2c579ed72ab16485e3d82
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-09-14 19:23:58 +0530

    Time: 4 ms (32.36%) | Memory: 9.9 MB (87.88%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,20 +11,50 @@
  */
 class Solution {
 public:
-void inorder(TreeNode* root,vector<int>&ans){
-    if(root==nullptr)return ;
-    inorder(root->left,ans);
-    ans.push_back(root->val);
-    inorder(root->right,ans);
-}
+
     vector<int> inorderTraversal(TreeNode* root) {
         
-        vector<int>ans;
-        inorder(root,ans);
-        return ans;
-        
+       vector<int>inorder;
+       TreeNode* cur =root;
+       while(cur!=nullptr){
+        if(cur->left==nullptr){
+            inorder.push_back(cur->val);
+            cur=cur->right;
+        }
+        else{
+            TreeNode* prev=cur->left;
+            while(prev->right && prev->right!=cur){
+                prev=prev->right;
+            }
+            if(prev->right==nullptr){
+                prev->right=cur;
+                cur=cur->left;
+            }
+            else{
+                prev->right=nullptr;
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+        }
+       }
+        return inorder;
     }
 };
+
+//  vector<int>ans;
+//         inorder(root,ans);
+//         return ans;
+
+
+
+// void inorder(TreeNode* root,vector<int>&ans){
+//     if(root==nullptr)return ;
+//     inorder(root->left,ans);
+//     ans.push_back(root->val);
+//     inorder(root->right,ans);
+// }
+
+
 
 // simple method using recursion only very easy
 // make function then call
