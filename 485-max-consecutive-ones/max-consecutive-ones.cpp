6e2250eefd6fcb5f485cb002a19633fcbfa66f65
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-10-02 16:54:00 +0530

    Time: 23 ms (88.01%) | Memory: 49.1 MB (14.64%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -7,6 +7,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (nums[i] == 1) {
                 cnt++;
+                
             } else if(nums[i]==0)cnt=0;
                 maxi = max(maxi, cnt);
         }
