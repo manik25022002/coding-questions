0310d77cc48728d541620501e8d979e1ee05863b
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-10-02 12:10:15 +0530

    Time: 34 ms (81.71%) | Memory: 37.6 MB (39.05%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> ans;
-
         int n = nums.size();
         vector<int> hash(n + 1, 0);
-        // int hash[n+1] = {0} ;
         for (int i = 0; i < n; i++)
             hash[nums[i]]++;
         for (int i = 1; i <=n; i++) {
