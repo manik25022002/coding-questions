56042ed0fa835b0efffcd2279812ba012c92406a
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-11-15 15:56:07 +0530

    Time: 15 ms (27.25%) | Memory: 21.8 MB (17.64%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findmaxi(vector<int>& v) {
+        int maxi = INT_MIN;
+        for (int i = 0; i < v.size(); i++) {
+            maxi = max(v[i], maxi);
+        }
+        return maxi;
+    }
+    long long findtotalhours(vector<int>& v, int perhour) {
+        long long totalhours = 0;
+        for (int i = 0; i < v.size(); i++) {
+             totalhours+=ceil((double)(v[i])/(double)(perhour));
+        }
+        return totalhours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int s = 1;
+        int e = findmaxi(piles);
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            long long val = findtotalhours(piles, mid);
+            if (val > h) {
+                s=mid+1;
+                // e = mid - 1;
+            } else {e = mid - 1;
+                // s = mid + 1;
+            }
+        
+        }
+        return s;
+    }
+};
