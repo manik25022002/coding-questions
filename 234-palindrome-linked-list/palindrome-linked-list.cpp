b4bff5409d89b97b4f85a7ab5f1e66751938e8d6
MANIK GARG <113615846+manik25022002@users.noreply.github.com>
2024-11-25 23:33:47 +0530

    Time: 3 ms (63.28%) | Memory: 120.6 MB (54.37%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,19 +10,54 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* temp = head;
+        while (temp != nullptr) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+
     bool isPalindrome(ListNode* head) {
-        stack<int>st;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            st.push(temp->val);
-            temp=temp->next;
+        if (head == nullptr || head->next == nullptr)
+            return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        temp=head;
-        while(temp!=nullptr){
-            if(temp->val != st.top())return false;
-            st.pop();
-            temp=temp->next;
+        ListNode* afterreverse = reverse(slow->next);
+        ListNode* second = afterreverse;
+        ListNode* first = head;
+        while (second != nullptr) {
+            if (first->val != second->val) {
+                reverse(afterreverse);
+                return false;
+            }
+            first = first->next;
+            second = second->next;
         }
+
+        reverse(afterreverse);
         return true;
     }
 };
+
+// stack<int>st;
+//         ListNode* temp=head;
+//         while(temp!=nullptr){
+//             st.push(temp->val);
+//             temp=temp->next;
+//         }
+//         temp=head;
+//         while(temp!=nullptr){
+//             if(temp->val != st.top())return false;
+//             st.pop();
+//             temp=temp->next;
+//         }
+//         return true;
